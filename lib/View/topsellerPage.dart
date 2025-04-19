fc123a6165c3f5baeca30801078d3900b8d4c606
VinaQhurotuAini <220711673@students.uajy.ac.id>
2025-04-20 01:20:37 +0700

    login,homePage baru

---
+++ lib/View/topsellerPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Topsellerpage extends StatefulWidget {
+  const Topsellerpage({super.key});
+
+  @override
+  State<Topsellerpage> createState() => _TopsellerpageState();
+}
+
+class _TopsellerpageState extends State<Topsellerpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
